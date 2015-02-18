434ada86e42c5bdf624511d6e60610fd750affd9
higex <popovici@iba.muni.cz>
2015-02-18 12:50:55 +0100

    mainly utilities

---
+++ stain/he.py
@@ -0,0 +1,104 @@
+"""
+HE module: responsible for color processing for images of H&E-stained tissue samples.
+"""
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+
+__version__ = 0.02
+__author__ = 'Vlad Popovici'
+__all__ = ['rgb2he', 'rgb2he2']
+
+import numpy as np
+# from scipy.linalg import norm
+
+# from scipy import linalg
+from skimage.color import separate_stains
+from skimage import img_as_ubyte
+from skimage.exposure import rescale_intensity
+
+def rgb2he(img, normalize=False):
+    """
+    RGB2HE: Extracts the haematoxylin and eosin components from an RGB color.
+
+    h,e = rgb2he(img, normalize=False)
+
+    Args:
+        img (numpy.ndarray): and RGB image; no check for actual color space is
+        performed
+        
+        normalize (bool): should the values be linearly transformed, to ensure
+        they are all between -1.0 and 1.0  
+
+    Returns:
+        tuple. Contains two intensity images as numpy.ndarray, coding for the
+        haematoxylin and eosin components, respectively. The values are in the
+        "H&E" space or in -1..1 (if normalize==True)
+    """
+
+    # my color separation matrices
+    # (from http://www.mecourse.com/landinig/software/colour_deconvolution.zip)
+    # The code commented out below was used to generate the "he1_from_rgb" matrix.
+    # After the matrix was obtained, it has been hard coded, for computation 
+    # efficiency:
+    # rgb_from_he1 = np.array([[0.644211, 0.716556, 0.266844],
+    #                         [0.092789, 0.954111, 0.283111],
+    #                         [0.0, 0.0, 0.0]])
+    # rgb_from_he1[2, :] = np.cross(rgb_from_he1[0, :], rgb_from_he1[1, :])
+    # he1_from_rgb = linalg.inv(rgb_from_he1)
+    he1_from_rgb = np.array([[ 1.73057512, -1.3257525 , -0.1577248 ],
+                             [-0.19972397,  1.1187028 , -0.48055639],
+                             [ 0.10589662,  0.19656106,  1.67121469]])
+
+    img_tmp = separate_stains(img_as_ubyte(img), he1_from_rgb)
+    
+    # The RGB -> H&E transformation maps:
+    # black (0,0,0)       |-> (-1.13450710, 0.00727017021)
+    # white (255,255,255) |-> (-9.08243792, 0.05.82022531)
+    # red   (255,0,0)     |-> (-9.53805685, 6.44503007)
+    # green (0,255,0)     |-> (-0.164661728, -5.42507111)
+    # blue  (0,0,255)     |-> (-1.64873355, -0.947216369)
+    
+    if normalize:
+        img_tmp[:,:,0] = 2*(img_tmp[:,:,0] + 9.53805685) / (-0.164661728 + 9.53805685) - 1
+        img_tmp[img_tmp[:,:,0] < -1.0, 0] = -1.0
+        img_tmp[img_tmp[:,:,0] >  1.0, 0] =  1.0
+        
+        img_tmp[:,:,1] = 2*(img_tmp[:,:,1] + 5.42507111) / (6.44503007 + 5.42507111) - 1
+        img_tmp[img_tmp[:,:,1] < -1.0, 0] = -1.0
+        img_tmp[img_tmp[:,:,1] >  1.0, 0] =  1.0
+        
+    return img_tmp[:, :, 0], img_tmp[:, :, 1]
+
+
+def rgb2he2(img):
+    # This implementation follows http://web.hku.hk/~ccsigma/color-deconv/color-deconv.html
+
+    assert (img.ndim == 3)
+    assert (img.shape[2] == 3)
+
+    height, width, _ = img.shape
+
+    img = -np.log((img + 1.0) / img.max())
+
+    # the following lines are replaced with the final result,
+    # to speed up computations
+    #
+    # he = np.array([0.550, 0.758, 0.351]); he /= norm(he)
+    # eo = np.array([0.398, 0.634, 0.600]); eo /= norm(eo)
+    # bg = np.array([0.754, 0.077, 0.652]); bg /= norm(bg)
+    #
+    # M = np.hstack((he.reshape(3,1), eo.reshape(3,1), bg.reshape(3,1)))
+    # D = alg.inv(M)
+    #
+    D = np.array([[ 1.92129515,  1.00941672, -2.34107612],
+                  [-2.34500192,  0.47155124,  2.65616872],
+                  [ 1.21495282, -0.99544467,  0.2459345 ]])
+
+    rgb = img.swapaxes(2, 0).reshape((3, height*width))
+    heb = np.dot(D, rgb)
+    res_img = heb.reshape((3, width, height)).swapaxes(0, 2)
+
+    return rescale_intensity(res_img[:,:,0], out_range=(0,1)), \
+           rescale_intensity(res_img[:,:,1], out_range=(0,1)), \
+           rescale_intensity(res_img[:,:,2], out_range=(0,1))
---
+++ util/intensity.py
@@ -0,0 +1,110 @@
+"""
+INTENSITY: utility functions for intensity transformation.
+
+@author: vlad
+"""
+__version__ = 0.01
+from cv2 import kmeans
+#from atk import Image
+
+__author__ = 'Vlad Popovici'
+__all__ = ['invert', '_R', '_G', '_B', 'requantize']
+
+import numpy as np
+from sklearn.cluster import MiniBatchKMeans
+from skimage.util import dtype_limits
+from skimage.exposure import rescale_intensity
+
+
+def invert(img, mx=None):
+    """
+    INVERT applies inverse video transformation on single channel images.
+
+    Usage:
+        res = invert(img, max_intensity)
+
+    Args:
+        img (numpy.ndarray): single channel image
+        max_intensity (implicit none): the maximum intensity of the type of
+            images provided as input (e.g. a 8 bit/pixel image has normally
+            max_intensity=255)
+
+    Returns:
+        numpy.ndarray: an image of the same shape as the original, but with
+        the intensity levels inverted (max_intensity - initial_value)
+
+    Raises:
+        ValueError: if the input image is not single channel
+    """
+
+    if img.ndim != 2:
+        raise ValueError('A single channel image is expected')
+
+    if mx is None:
+        mx = dtype_limits(img)[1]
+    else:
+        mx = np.min(dtype_limits(img)[1], mx)
+
+    res = mx - img
+
+    return res
+
+
+###
+# Aliases for color channels:
+def _R(_img):
+    return _img[:,:,0]
+
+def _G(_img):
+    return _img[:,:,1]
+
+def _B(_img):
+    return _img[:,:,2]
+
+
+def requantize(img, nlevels=2, method='linear'):
+    """
+    REQUANTIZE: changes the number of grey scale levels in an image.
+
+    Usage:
+        res = requantize(img, nlevels=2)
+
+    Args:
+        img (numpy.ndarray): a single channel image
+        nlevels (int): number of levels in the resulting image (<256)
+        method (string): 'linear' or 'adaptive'
+            'linear': the interval 0..max(dtype) is split into nlevels
+            equal length intervals onto which input values are mapped
+            'kmeans': vector quantization is applied to determine
+            the new quantification levels
+
+    Returns:
+        numpy.ndarray: a single image of type uint8
+
+    Raises:
+        ValueError: if the image is not single channel, or if invalid
+        arguments are given
+    """
+
+    if img.ndim != 2:
+        raise ValueError('A single channel image is expected')
+
+    assert(1 < nlevels <= 256 )
+    assert(method.lower() in ['linear', 'kmeans'])
+
+    if method.lower() == 'linear':
+        res = np.ndarray(img.shape, dtype=np.uint8)
+        res.fill(nlevels - 1)                        # nlevels-1 is the highest grey level
+        limits = np.linspace(0, 256, nlevels+1, dtype=np.uint8)
+        img = rescale_intensity(img, out_range=(0, 255))
+        for k in np.arange(start=nlevels-2, stop=-1, step=-1):
+            res[img < limits[k]] = k
+    elif method.lower() == 'kmeans':
+        vq = MiniBatchKMeans(n_clusters=nlevels)
+        vq.fit(img.reshape((-1,1)))
+        res = vq.labels_.reshape(img.shape).astype(np.uint8)
+    else:
+        ValueError('Incorrect method specified')
+
+    return res
+
